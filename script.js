d10b0d8c27c1c61f411f0f028b1fd6a8dbb9427d
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-09-24 16:35:45 -0700

    added rows and time to html

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log(moment().format('dddd,MMM Do'));
+//  document.querySelector(".display-3").textContent = "hello";
+
+// $("h2").text("hi"); 
+
+$("#currentDay").text(moment().format('dddd,MMM Do'));
+
