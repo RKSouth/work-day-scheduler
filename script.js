eca3c0648ae00bb7f5bac7199d840a81c0f22f9b
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-09-24 23:59:18 -0700

    edited css to match the gif

--- script.js
+++ script.js
@@ -27,64 +27,29 @@ moment().format('MMMM Do YYYY, h:mm:ss a');
 //change the color
 var time =[9,10,11,12,13,14,15,16,17,18];
 var classIds = ['#9','#10','#11','#12','#13','#14','#15', '#16','#17','#18'];
-var present=parseInt(moment().format('h'));
+var present=12;
+// parseInt(moment().format('h'));
 console.log(present);
-// for(i=0;
-//      i<present.time; i++){
-//     console.log("loop");
-//     console.log(parseInt($('#9')));
-// //     if(time[i] === parseInt(classIds[i])){
-// // $(".textarea" ).add( document.getElementsByTagName( "textarea" )[ i ] )
-// //   .css( "background-color", "red" );
-// //     }
-// }
-for (i=0; i< time[9]; i++){
+
+for (i=0; i< time[10]; i++){
 if(i<present){
-//     $(".textarea" ).add( document.getElementsByTagName( "textarea" )[ 0 ] )
-//   .css( "background-color", "red" );
+
 $('.present').removeClass('present').addClass('past');
 }}
- for (i=0; i< time[9]; i++){
+ for (i=0; i< time[10]; i++){
 if(i==present){
-//     $(".textarea" ).add( document.getElementsByTagName( "textarea" )[ 0 ])
-//   .css( "background-color", "blue" );
+
    $('.future').removeClass('future').addClass('present');
     
 } else( console.log("false")) };
-for (i=0; i< time[9]; i++){
-if(i>present){
-//     $(".textarea" ).add( document.getElementsByTagName( "textarea" )[ 2 ] )
-//   .css( "background-color", "gray" );
-    
-}
-};
+// for (i=0; i< time[9]; i++){
+// if(i>present){
+
+//     $('.past').removeClass('future').addClass('future');
+// }
+// };
 
 console.log(present);
 console.log($("#10"));
-// if ( $("#") == present) {
-// $(".textarea").text("present");
-// console.log("present time is working")
-// };
-// var past = present -1
-// console.log(past)
-// if ($("#9").text() == past) {
-//     html.find('textarea').change("#present");
-//     console.log("past time is working")
-//     };
-// var future = present +1
-// if ($(".hour").text() == present) {
-//         $(".textarea").text("present");
-//         console.log("future time is working")
-//         };
-// function togglePast() {$("div.textarea.j").replaceWith(".past")};
 
-
-// for(i=0; i<time.length; i++) {
-// if (i < present && parseInt(classIds)===i) {
-//     console.log("past time is working");
-//      console.log(this);
-//     $(".textarea").add.class("present");
-
-
-// togglePast();
-});
+});//end of document
