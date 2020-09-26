744975d79984fa53a3618cffaf0805f35b50a7da
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-09-26 13:47:16 -0700

    code is functional?

--- script.js
+++ script.js
@@ -4,16 +4,14 @@ $(document).ready(function(){
  // Static
 
     // The first basic method of Moment.js is the moment() method. Use this to get today's info!
-    // console.log(moment());
-
-    // console.log(moment().format());
+   
 
     // Using moment format, there are different ways that we can display todays date!
 
     // Current Time
     // Here we want to use setInterval to constantly update the time
     let updateTime = function () {
-        let currentTime = moment().format('MMMM Do YYYY, h:mm:ss a')
+        let currentTime = moment().format('MMMM Do YYYY')
         $("#time").text(currentTime)
     }
 
@@ -27,61 +25,61 @@ $(document).ready(function(){
     }
 
     // Here we will get the number of minutes in the week
-    let updateMinutes = function () {
-        let date = moment().weekday();
-        let weekMinutes = (date * 24 * 60) + (moment().minute());
-        // $("#minutes").text(weekMinutes);
-    }
+    // let updateMinutes = function () {
+    //     let date = moment().weekday();
+    //     let weekMinutes = (date * 24 * 60) + (moment().minute());
+    //     // $("#minutes").text(weekMinutes);
+    // }
 
-    // Here we will get the number of seconds in the day
-    let updateSeconds = function () {
-        let todayHours = moment().hour();
-        let todaySeconds = todayHours * 60 * 60;
-        let thisSecond = moment().second();
-        let sumSeconds = thisSecond + todaySeconds;
-        // $("#seconds").text(sumSeconds);
+    // // Here we will get the number of seconds in the day
+    // let updateSeconds = function () {
+    //     let todayHours = moment().hour();
+    //     let todaySeconds = todayHours * 60 * 60;
+    //     let thisSecond = moment().second();
+    //     let sumSeconds = thisSecond + todaySeconds;
+    //     // $("#seconds").text(sumSeconds);
     
-    }
+    // }
 
-    let countdown = function () {
+    // let countdown = function () {
 
-        // Friday
-        let friday = moment().day("fr")._d
+    //     // Friday
+    //     let friday = moment().day("fr")._d
 
-        // convert this into day of the year
-        let fridayDate = moment(friday).dayOfYear();
+    //     // convert this into day of the year
+    //     let fridayDate = moment(friday).dayOfYear();
 
-        // This will get the milliseconds of Friday
-        let fridayMils = fridayDate * 24 * 60 * 60 * 1000
-        // Turn into seconds
+    //     // This will get the milliseconds of Friday
+    //     let fridayMils = fridayDate * 24 * 60 * 60 * 1000
+    //     // Turn into seconds
 
-        // Today
-        // Turn into milliseconds
+    //     // Today
+    //     // Turn into milliseconds
 
-        let date = moment().dayOfYear()
-        let yearMils = date * 24 * 60 * 60 * 1000
-        let todayMils = moment().hour() * 60 * 60 * 1000;
-        let thisMilsSecond = moment().second() * 1000;
-        let thisMils = moment().millisecond();
-        // This value should be increasing over time
-        let sumMils = yearMils + todayMils + thisMilsSecond + thisMils;
+    //     let date = moment().dayOfYear()
+    //     let yearMils = date * 24 * 60 * 60 * 1000
+    //     let todayMils = moment().hour() * 60 * 60 * 1000;
+    //     let thisMilsSecond = moment().second() * 1000;
+    //     let thisMils = moment().millisecond();
+    //     // This value should be increasing over time
+    //     let sumMils = yearMils + todayMils + thisMilsSecond + thisMils;
 
-        let ms = fridayMils - sumMils;
-        $("#countdown").text(ms)
-    }
+    //     let ms = fridayMils - sumMils;
+    //     $("#countdown").text(ms)
+    // }
 
     // To initally set the times, we will call the functions
     updateTime();
     updateHour();
-    updateMinutes();
-    updateSeconds();
-    countdown();
+    // updateMinutes();
+    // updateSeconds();
+    // countdown();
 
     // To continuously call the functions, we will use setInterval
     setInterval(updateTime, 1000);
     setInterval(updateHour, 1000);
-    setInterval(updateMinutes, 1000);
-    setInterval(updateSeconds, 1000);
+    // setInterval(updateMinutes, 1000);
+    // setInterval(updateSeconds, 1000);
     
 
 //how does the timer work??
@@ -122,26 +120,28 @@ if(time[i] < present ){
     $(textIds[i]).removeClass('future').addClass('past');
 }
 };
-allDbtns = ["#btn9a","#btn10a","#btn11a","#btn12p","#btn1p","#btn2p","#btn3p","#btn4p","#btn5p"]
-
+allDbtns = ["#btn9a","#btn10a","#btn11a","#btn12p","#btn1p","#btn2p","#btn3p","#btn4p","#btn5p"];
+console.log(allDbtns.indexOf("#btn9a"));
 // local storage
-for (i=0; i< 10; i++){
-loopArray=[]
-var textInput;
-saveArray=[];
-var storedT =  $("#9").text();
-console.log($("#9").text());
 
-if (allDbtns[i] === "#btn9a"){
+var textInput;
+console.log(allDbtns.indexOf(i));
+saveArray=[];
+
+//All the buttons,
+// was going to put in a for loop but when I did I got errors
+//error 1 console.log(allDbtns.indexOf(i)); is -1??
+// error 2 the text input is undefined
 $("#btn9a").on("click", function(){
     alert("The paragraph was clicked.");
    
     textInput = $("#9").val();
     console.log(textInput);
+
     //take out old text from array, add new text
-    saveArray.shift(textInput);
+    saveArray.shift("#btn9a");
     saveArray.push(textInput);
-    console.log($(this.saveArray));
+
     //take out old text from local storage, add new text
     localStorage.removeItem("#btn9a",saveArray);
     localStorage.setItem("#btn9a",saveArray);
@@ -150,10 +150,167 @@ $("#btn9a").on("click", function(){
     junction();
   
 });
+
+$("#btn10a").on("click", function(){
+    alert("The paragraph was clicked.");
+   
+    textInput = $("#10").val();
+    console.log(textInput);
+
+    //take out old text from array, add new text
+    saveArray.shift("#btn10a");
+    saveArray.push(textInput);
+
+    //take out old text from local storage, add new text
+    localStorage.removeItem("#btn10a",saveArray);
+    localStorage.setItem("#btn10a",saveArray);
+    // console.log(localStorage.getItem("#btn9a"));
+    // storedT = localStorage.getItem("#btn9a",JSON.parse(saveArray));
+    junction();
+  
+});
+
+$("#btn11a").on("click", function(){
+    alert("The paragraph was clicked.");
+   
+    textInput = $("#11").val();
+    console.log(textInput);
+
+    //take out old text from array, add new text
+    saveArray.shift("#btn11a");
+    saveArray.push(textInput);
+
+    //take out old text from local storage, add new text
+    localStorage.removeItem("#btn11a",saveArray);
+    localStorage.setItem("#btn11a",saveArray);
+    // console.log(localStorage.getItem("#btn9a"));
+    // storedT = localStorage.getItem("#btn9a",JSON.parse(saveArray));
+    junction();
+  
+});
+
+$("#btn12p").on("click", function(){
+    alert("The paragraph was clicked.");
+   
+    textInput = $("#12").val();
+    console.log(textInput);
+
+    //take out old text from array, add new text
+    saveArray.shift("#btn12p");
+    saveArray.push(textInput);
+
+    //take out old text from local storage, add new text
+    localStorage.removeItem("#btn12p",saveArray);
+    localStorage.setItem("#btn12p",saveArray);
+    // console.log(localStorage.getItem("#btn9a"));
+    // storedT = localStorage.getItem("#btn9a",JSON.parse(saveArray));
+    junction();
+  
+});
+
+$("#btn1p").on("click", function(){
+    alert("The paragraph was clicked.");
+   
+    textInput = $("#13").val();
+    console.log(textInput);
+
+    //take out old text from array, add new text
+    saveArray.shift("#btn1p");
+    saveArray.push(textInput);
+
+    //take out old text from local storage, add new text
+    localStorage.removeItem("#btn1p",saveArray);
+    localStorage.setItem("#btn1p",saveArray);
+    // console.log(localStorage.getItem("#btn9a"));
+    // storedT = localStorage.getItem("#btn9a",JSON.parse(saveArray));
+    junction();
+  
+});
+
+$("#btn2p").on("click", function(){
+    alert("The paragraph was clicked.");
+   
+    textInput = $("#14").val();
+    console.log(textInput);
+
+    //take out old text from array, add new text
+    saveArray.shift("#btn2p");
+    saveArray.push(textInput);
+
+    //take out old text from local storage, add new text
+    localStorage.removeItem("#btn2p",saveArray);
+    localStorage.setItem("#btn2p",saveArray);
+    // console.log(localStorage.getItem("#btn9a"));
+    // storedT = localStorage.getItem("#btn9a",JSON.parse(saveArray));
+    junction();
+  
+});
+
+$("#btn3p").on("click", function(){
+    alert("The paragraph was clicked.");
+   
+    textInput = $("#15").val();
+    console.log(textInput);
+
+    //take out old text from array, add new text
+    saveArray.shift("#btn3p");
+    saveArray.push(textInput);
+
+    //take out old text from local storage, add new text
+    localStorage.removeItem("#btn3p",saveArray);
+    localStorage.setItem("#btn3p",saveArray);
+    // console.log(localStorage.getItem("#btn9a"));
+    // storedT = localStorage.getItem("#btn9a",JSON.parse(saveArray));
+    junction();
+  
+});
+
+$("#btn4p").on("click", function(){
+    alert("The paragraph was clicked.");
+   
+    textInput = $("#16").val();
+    console.log(textInput);
+
+    //take out old text from array, add new text
+    saveArray.shift("#btn4p");
+    saveArray.push(textInput);
+
+    //take out old text from local storage, add new text
+    localStorage.removeItem("#btn4p",saveArray);
+    localStorage.setItem("#btn4p",saveArray);
+    // console.log(localStorage.getItem("#btn9a"));
+    // storedT = localStorage.getItem("#btn9a",JSON.parse(saveArray));
+    junction();
+  
+});
+
+$("#btn5p").on("click", function(){
+    alert("The paragraph was clicked.");
+   
+    textInput = $("#17").val();
+    console.log(textInput);
+
+    //take out old text from array, add new text
+    saveArray.shift("#btn5p");
+    saveArray.push(textInput);
+
+    //take out old text from local storage, add new text
+    localStorage.removeItem("#btn5p",saveArray);
+    localStorage.setItem("#btn5p",saveArray);
+    // console.log(localStorage.getItem("#btn9a"));
+    // storedT = localStorage.getItem("#btn9a",JSON.parse(saveArray));
+    junction();
+  
+});
+
+
 function junction () {
-$("#9").text(localStorage.getItem("#btn9a"));
+    for(i=0; i<10; i++){
+$(textIds[i]).text(localStorage.getItem(allDbtns[i]));
+
+    }
 };
 junction();
 // local storage
-}}
+
 });//end of document
